ab30cb62124493e56548887184026fdbae4f431f
ahanrcs <ahsancs004.com>
2024-01-23 23:08:35 +0500

    CreaTE Pages and Routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home.jsx';
+import Profile from './pages/Profile.jsx';
+import Offers from './pages/Offers.jsx';
+import SignIn from './pages/SignIn.jsx';
+import SignUp from './pages/SignUp.jsx';
+import ForgotPassword from "./pages/ForgotPassword.jsx";
+
 function App() {
   return (
-    <h1 className='text-red-600 bg-black text-2xl' >This is heading</h1>
+    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/offers" element={<Offers/>}/>
+        <Route path="/forgot-password" element={<ForgotPassword/>}/>
+      </Routes>
+    </Router>
+    </>
   );
 }
 
