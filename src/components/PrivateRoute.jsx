32650badd863642732f0c78189433ef01f2f2874
ahanrcs <ahsancs004.com>
2024-02-05 09:58:18 +0500

    Sippner Cmompponent

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,10 +1,11 @@
 import { Navigate, Outlet } from "react-router";
+import Spinner from "./Spinner";
 import {useAuthStatus} from "./hooks/useAuthStatus";
 
 export default function PrivateRoute() {
     const {loggedin,checkingStatus} = useAuthStatus();
     if(checkingStatus){
-        return <h3>Loading</h3>
+        return <Spinner/>
     }
   return loggedin ? <Outlet/>: <Navigate to={'/sign-in'} />
 
