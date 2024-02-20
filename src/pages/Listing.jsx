e0381c23ec47cc47b4dfa65b0b7b981837453669
ahanrcs <ahsancs004.com>
2024-02-20 22:47:20 +0500

    Add Listing information to listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,10 +1,13 @@
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc} from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 import { useParams } from "react-router";
 import { db } from "../firebase";
 import { useEffect, useState } from "react";
 import Spinner from "../components/Spinner";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
+import { FaLocationDot } from "react-icons/fa6";
+import { FaBed, FaBath,FaParking,FaChair} from "react-icons/fa";
 import {
   EffectFace,
   Autoplay,
@@ -16,6 +19,7 @@ import "swiper/css/bundle";
 import { FaShare } from "react-icons/fa";
 
 export default function Listing() {
+  const auth = getAuth();
   const params = useParams();
   const [listing, setListing] = useState();
   const [loading, setLoading] = useState(true);
@@ -58,19 +62,78 @@ export default function Listing() {
           </SwiperSlide>
         ))}
       </Swiper>
-      <div className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center "
-       onClick={()=>{
-        navigator.clipboard.writeText(window.location.href);
-        setShareLink(true);
-        setTimeout(()=>{
-          setShareLink(false);
-        },2000)
-       }}> 
-      <FaShare className="text-lg text-slate-500"/>
+      <div
+        className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center "
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLink(true);
+          setTimeout(() => {
+            setShareLink(false);
+          }, 2000);
+        }}
+      >
+        <FaShare className="text-lg text-slate-500" />
       </div>
       {shareLink && (
-        <p className="fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2">Link Copied</p>
+        <p className="fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2">
+          Link Copied
+        </p>
       )}
+      <div className="m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4  rounded-lg border-3 shadow-lg bg-white lg:space-x-4">
+        <div className=" w-full h-[200px] lg--[400px]">
+          <p className="text-2xl font-bold mb-3 text-blue-900">
+            {listing.fullname}-${" "}
+            {listing.offer
+              ? listing.discountedPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+              : listing.regularPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                  {listing.type === "rent" ? " / Month" : " "}
+          </p>
+          <p className="flex items-center mt-6 mb-3 font-semibold">
+            <FaLocationDot className="text-green-700 mr-1 "/>
+              {listing.address}
+          </p>
+          <div className="flex justify-start items-center space-x-4 w-[75%]">
+            <p className="bg-red-800 w-full max-w-[200px] rounded-md p-1 text-white text-center font-semibold shadow-md">{listing.type ===  "rent" ? "Rent" : "Sale"}</p>
+            {listing.offer && (<p className="w-full max-w-[200px] bg-green-800 rounded-md p-1 text-white text-center font-semibold shadow-md">${+listing.regularPrice- +listing.discountedPrice} Discounted</p>)}
+          </div>
+          <p className="mt-3 mb-3">
+            <span className="font-semibold"> 
+            Dsscription - </span>
+            {listing.description}
+          </p>
+          <ul className="flex items-center space-x-2 sm:space-x-10 text-sm font-semibold">
+            <li className="flex items-center">
+            <FaBed className="text-lg mr-1" />
+              {+listing.bedroooms > 1 ? `${listing.bedroooms} Beds` : "1 Bed"}
+            </li>
+            <li className="flex items-center">
+            <FaBath className="text-lg mr-1" />
+              {+listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : "1 Bath"}
+            </li>
+            <li className="flex items-center">
+            <FaParking className="text-lg mr-1" />
+              {listing.parking? "Parking Spot" : "No Parking"}
+            </li>
+            <li className="flex items-center">
+            <FaChair className="text-lg mr-1" />
+            {listing.furnshid? "Furnished" : "Not Furnished"}
+            </li>
+          </ul>
+          {listing.userRef !== auth.currentUser?.uid && (
+            <div className="mt-6">
+              <button className="px-7 py-3 bg-blue-600 text-white font-medium text-sm uppercase rounded shadow-md 
+              hover:bg-blue-700 hover:shadow-lg focus::bg-blue-700 focus:shadow-lg  w-full text-center transition duration-150 ease-in-out">
+                Contact Landlord
+              </button>
+            </div>
+          )}
+        </div>
+        <div className="bg-blue-300 w-full h-[200px] lg--[400px] z-10 overflow-x-hidden"></div>
+      </div>
     </main>
   );
 }
